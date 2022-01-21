4e0f5e63dd6ede01696c61c4084788801bc66368
xyzroller <xyzroller@rollyourown.xyz>
2022-01-21 17:20:46 +0100

    Add non-default webhook path

--- image-build/playbooks/roles/set-up-webhook/files/start-webhook.sh
+++ image-build/playbooks/roles/set-up-webhook/files/start-webhook.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Start webhook
-/usr/local/bin/webhook -port 80 -hooks /usr/local/bin/hooks.yaml
+/usr/local/bin/webhook -port 80 -urlprefix "{{ project_id }}" -hooks /usr/local/bin/hooks.yaml
