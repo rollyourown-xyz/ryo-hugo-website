9c5816e1ce366c1b5ed7c916a61fc2e45701a74f
xyzroller <xyzroller@rollyourown.xyz>
2022-05-01 13:04:38 +0200

    Bump hugo version to v0.98.0

--- scripts-project/build-image-project.sh
+++ scripts-project/build-image-project.sh
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # Default software package versions
-hugo_version="0.97.3"
+hugo_version="0.98.0"
 oauth2_proxy_version="7.2.1"
 webhook_version="2.8.0"
 
